a7a9cbff2ca93768a4e639cca5c0fe7d043b2811
Joyce Zhang <jzzhang.dev@gmail.com>
2023-05-05 22:01:40 -0400

    Add speaker works

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -138,3 +138,41 @@
     
 })(jQuery);
 
+document.querySelector('#speakerAdd').addEventListener('click', addCharacter)
+const speakersGallery = document.getElementById('speakers');
+
+function addCharacter(){
+
+  const newGalleryItem = document.createElement('div');
+  newGalleryItem.classList.add('col-md-6', 'col-lg-4', 'col-xl-3');
+  newGalleryItem.innerHTML = `
+    <div class="speakers-gallery-item" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="400">
+      <div class="speakers-gallery-item-thumb overflow-hidden position-relative">
+        <img src="assets/images/gallery/gallery1.jpg" alt="Gallery Image 1">
+      </div>
+      <div class="">
+        <ul class="social-icons social">
+          <li>
+            <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-linkedin-in"></i></a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-twitter"></i></a>
+          </li>
+        </ul>
+      </div>
+      <div class="item-content">
+        <h3 class="title">New Speaker Name</h3>
+        <span class="sub">New Speaker Title</span>
+      </div>
+    </div>
+  `;
+  
+  // add new gallery item to speakers gallery
+  speakersGallery.querySelector('.speakers-gallery-items-wrap .row').appendChild(newGalleryItem);
+
+  console.log('it works')
+
+}
